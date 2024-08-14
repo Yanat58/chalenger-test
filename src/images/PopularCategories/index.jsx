5cffbc9400271949afc2455f73e08a97543a77d9
Karlova Tetiana <tanyalyt80@gmail.com>
2024-08-14 16:00:16 +0300

    created a folder PopularCategories

---
+++ src/images/PopularCategories/index.jsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css'
+
+export default function PopularCategories() {
+  return (
+    <section className={styles.container}>
+     <h2 className={styles.title}>Популярні категорії</h2>
+    </section>
+  )
+}
