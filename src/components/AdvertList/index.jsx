94da8eaf7f34fab9fda63bb5c1a903498518137f
Karlova Tetiana <tanyalyt80@gmail.com>
2024-08-18 21:50:59 +0300

    finish PopularCategories

--- src/components/AdvertList/index.jsx
+++ src/components/AdvertList/index.jsx
@@ -1,39 +1,50 @@
-import { useState } from "react";
-import styles from "./styles.module.css";
-import { advers } from "./advers";
-import { FaRegHeart, FaHeart } from "react-icons/fa";
-import photoAlternate from '../../images/not-found.png'
-
+import { useState } from 'react';
+import styles from './styles.module.css';
+import { advers } from './advers';
+import { FaRegHeart, FaHeart } from 'react-icons/fa';
+import photoAlternate from '../../images/not-found.png';
 
 export default function AdvertList() {
   const [favorite, setFavorite] = useState(false);
-  console.log(advers)
 
   return (
     <section className={styles.container}>
       <h2 className={styles.title}>VIP оголошення</h2>
       <ul className={styles.list}>
         {advers?.map((item, index) => (
-          <li className={styles.item} key={index} >
+          <li className={styles.item} key={index}>
             <p className={styles.category}>VIP-оголошення</p>
-            <img className={styles.img} src={item.img ? item.img : photoAlternate} alt="" />
+            <img
+              className={styles.img}
+              src={item.img ? item.img : photoAlternate}
+              alt=""
+            />
             <button
               className={styles.heart_btn}
               type="button"
               onClick={() => setFavorite(!favorite)}
             >
-              {favorite ? <FaHeart color="blue" /> : <FaRegHeart color="#000" />}
+              {favorite ? (
+                <FaHeart color="blue" />
+              ) : (
+                <FaRegHeart color="#000" />
+              )}
             </button>
             <div className={styles.box_text}>
-              <p className={styles.span}><span>Опубліковано: 12.07.2024</span></p>
-              <p className={styles.text}>Українська традиційна вишиванка жіночка Львівська</p>
-              <p className={styles.span}><span>Lara_Sylwer25</span></p>
+              <p className={styles.span}>
+                <span>Опубліковано: 12.07.2024</span>
+              </p>
+              <p className={styles.text}>
+                Українська традиційна вишиванка жіночка Львівська
+              </p>
+              <p className={styles.span}>
+                <span>Lara_Sylwer25</span>
+              </p>
               <p className={styles.price}>850 грн</p>
             </div>
-          </li>))}
+          </li>
+        ))}
       </ul>
-
     </section>
-
   );
 }
